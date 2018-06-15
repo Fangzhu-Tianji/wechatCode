25d22e5ac49a0d3cf49e1a43661f449a89caba47
放逐天际 <793262201@qq.com>
2018-06-15 18:09:40 +0800

    电影样式

--- readerMovie/pages/movies/movies.js
+++ readerMovie/pages/movies/movies.js
@@ -12,7 +12,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url: 'https://douban.uieee.com/v2/movie/top250', //仅为示例，并非真实的接口地址
+      data: {},
+      method: 'GET',
+      header: {
+        "Content-Type": "application"
+      },
+      success: function (res) {
+        console.log(res)
+      }
+    })
+
   },
 
   /**
