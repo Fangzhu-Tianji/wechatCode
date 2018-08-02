44d3d8dc5d2b1a7e7a17822cb9f658573aa972e4
放逐天际 <793262201@qq.com>
2018-08-02 20:03:45 +0800

    完善分类，增加购物车

--- fhTea/pages/category/category.js
+++ fhTea/pages/category/category.js
@@ -7,13 +7,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    switchItem: true, //切换排列方式
+    showShades: false, //是否显示筛选内容
+    contentItemId: 0
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log(util)
+  },
+  //点击切换排列结构
+  onSwitch: function () {
+    this.setData({ switchItem: !this.data.switchItem });
   },
   //点击扫码
   onSweep: function () {
@@ -31,5 +37,31 @@ Page({
     wx.navigateTo({
       url: '../search/search/search'
     })
+  },
+  //显示筛选遮罩
+  switchShade: function () {
+    this.setData({ showShades: !this.data.showShades });
+  },
+  //禁止滚动穿透
+  touchMove: function () {
+    return;
+  },
+  //上拉加载更多
+  onReachBottom: function () {
+    console.log('到底部了');
+  },
+  //点击筛选请求数据
+  requestData: function (event) {
+    var _this = this;
+    var id = event.currentTarget.dataset.id;
+    if (id == _this.data.contentItemId) {
+      _this.setData({ contentItemId: 0 });
+    }
+    else {
+      _this.setData({ contentItemId: event.currentTarget.dataset.id });
+    }
+    setTimeout(function () {
+      _this.switchShade();
+    }, 200)
   }
 })
