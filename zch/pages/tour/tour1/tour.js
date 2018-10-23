e6eed3cd87a0f5b3dd193890fc070fbcc16cb114
放逐天际 <793262201@qq.com>
2018-10-23 19:09:48 +0800

    音乐

--- zch/pages/tour/tour1/tour.js
+++ zch/pages/tour/tour1/tour.js
@@ -1,66 +1,67 @@
-// pages/tour/tour1/tour.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    music: 'play'
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 页面进入时音乐自动播放
+    wx.playBackgroundAudio({
+      dataUrl: 'http://mp3file.mafengwo.net/201810241750/c53f57052352a619b426ee4e37a2f5ea/s5/M00/1C/2C/wKgB3FCkRdaABP1rAD2RzZolouU532.mp3'
+    })
+    // 监听音乐变化
+    this.setMusicMonitor();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 设置音乐
+  setMusicMonitor: function () {
+    var _this = this;
+    wx.onBackgroundAudioPlay(function () {
+      _this.setData({
+        music: 'begin_music'
+      })
+      setTimeout(function () {
+        _this.setData({
+          music: 'play'
+        })
+      }, 500);
+      //开始音乐播放
+      wx.playBackgroundAudio({
+        dataUrl: 'http://mp3file.mafengwo.net/201810241750/c53f57052352a619b426ee4e37a2f5ea/s5/M00/1C/2C/wKgB3FCkRdaABP1rAD2RzZolouU532.mp3'
+      })
+    });
+    wx.onBackgroundAudioPause(function () {
+      _this.setData({
+        music: 'pause'
+      })
+      wx.pauseBackgroundAudio();//暂停音乐播放
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 控制音乐
+  controlMusic: function () {
+    var _this = this;
+    if(this.data.music == 'play') {
+      this.setData({
+        music: 'pause'
+      })
+      wx.pauseBackgroundAudio();//暂停音乐播放
+    }
+    else {
+      this.setData({
+        music: 'begin_music'
+      })
+      setTimeout(function(){
+        _this.setData({
+          music: 'play'
+        })
+      },500);
+      //开始音乐播放
+      wx.playBackgroundAudio({
+        dataUrl: 'http://mp3file.mafengwo.net/201810241750/c53f57052352a619b426ee4e37a2f5ea/s5/M00/1C/2C/wKgB3FCkRdaABP1rAD2RzZolouU532.mp3'
+      })
+    }
   }
 })
